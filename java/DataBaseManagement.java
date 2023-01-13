655af95cf5c9e6e299ac693650bef4ba4f5dccee
omkar <omkardudhewar121@gmail.com>
2023-01-13 14:09:05 +0530

    added: create payroll database

--- java/DataBaseManagement.java
+++ java/DataBaseManagement.java
@@ -1,5 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseManagement {
+    private static final String url = "jdbc:mysql://localhost:3306/employee_payroll";
+    private static final String userName = "root";
+    private static final String password = "Omkar@12";
+    static Connection connection;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to DBMS");
+        try {
+            connection = DriverManager.getConnection(url, userName, password);
+            if (connection != null) {
+                System.out.println("Database Connected Successfully!!!");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Exception Occurs" + e);
+        }
     }
 }
