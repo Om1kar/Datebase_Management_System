c0eb580cc9718be4c20557a7dab7d8e0b08db6ef
omkar <omkardudhewar121@gmail.com>
2023-01-13 15:13:04 +0530

    added: update salary for Terisa

--- java/DataBaseManagement.java
+++ java/DataBaseManagement.java
@@ -1,14 +1,16 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class DataBaseManagement {
-    private static final String url = "jdbc:mysql://localhost:3306/employee_payroll";
-    private static final String userName = "root";
-    private static final String password = "Omkar@12";
-    static Connection connection;
+    public static final String url = "jdbc:mysql://localhost:3306/employee_payroll";
+    public static final String userName = "root";
+    public static final String password = "Omkar@12";
 
-    public static void main(String[] args) {
+    public static void dataBaseManagement() {
+        Connection connection;
         try {
             connection = DriverManager.getConnection(url, userName, password);
             if (connection != null) {
@@ -18,5 +20,26 @@ public class DataBaseManagement {
         } catch (SQLException e) {
             System.out.println("Exception Occurs" + e);
         }
+    }
+
+    public static void updateSalary() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            PreparedStatement prepare = connection.prepareStatement("update EmployeePayroll set basicpay = ? where id = ?;");
+            System.out.print("Enter id = ");
+            int id = scanner.nextInt();
+            System.out.print("Enter basic pay = ");
+            int basicpay = scanner.nextInt();
+            prepare.setInt(1, basicpay);
+            prepare.setInt(2, id);
+            prepare.executeLargeUpdate();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        dataBaseManagement();
+        updateSalary();
     }
 }
